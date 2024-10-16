315f508490e9d4e8db552e1218f19b52335ec75e
Klever Gopar <20468297+klever12@users.noreply.github.com>
2024-10-16 07:15:40 +0000

    implemented test for test_s method of Animal class

--- tests/test_animal.rb
+++ tests/test_animal.rb
@@ -3,7 +3,7 @@ require "./animal.rb"
 
 class TestAnimal < Minitest::Test
   def test_to_s
-      new_animal = Animal.new("Monkey", "George")
-      assert_equal 
+      new_animal = Animal.new("monkey", "George")
+      assert_equal "George the monkey", new_animal.to_s, "to_s test method failed"
   end
 end
