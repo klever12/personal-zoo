5e3fb79e693097a8e7f5d6d9aea22acbc4b0d340
Klever Gopar <20468297+klever12@users.noreply.github.com>
2024-10-16 05:22:55 +0000

    implemented welcome method to PersonalZoo class

--- app.rb
+++ app.rb
@@ -2,4 +2,4 @@ require "./personal_zoo.rb"
 
 # create new personal zoo and call manage method
 personal_zoo = PersonalZoo.new
-personal_zoo.manage
+personal_zoo.welcome
--- personal_zoo.rb
+++ personal_zoo.rb
@@ -6,4 +6,14 @@ class PersonalZoo
   def initialize
     @animals = []
   end
+
+  def welcome
+    # welcome messages for user 
+    puts ""
+    puts "Welcome to your personal zoo!"
+    puts "Your job is to add unique animals and we'll handle adding them companions."
+    puts "You're also allowed to name the unique animal you add, and we'll be naming its friends."
+    puts "Lets begin your zoo management!"
+
+  end
 end
